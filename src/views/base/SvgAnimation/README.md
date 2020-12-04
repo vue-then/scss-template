ed154cb56095db3bf24c607e8f60775afb057db3
yyccQQu <643929860@qq.com>
2020-12-04 21:24:29 +0400

    svg 1

---
+++ src/views/base/SvgAnimation/README.md
@@ -0,0 +1,19 @@
+### 总结 svg 特点
+- SVG 与 PNG、JPEG 和 GIF 图片相比，拥有很小的尺寸；
+- SVG 是矢量图，可以伸缩，适用各种分辨率；
+- SVG 采用开放标准，本质是 XML，可以被各种工具读取（浏览器、图片管理器、markdown 等）；
+- SVG 图像中的文本是可选的，同时也是可搜索的；
+- SVG 可以与 CSS 结合，获得更强的扩展性。
+
+- path的用法 https://www.w3school.com.cn/svg/svg_path.asp
+- tips
+```
+实现细节：
+    rotate: auto -> 0 可以更加流畅
+    begin: 可填入多个值，支持表达式
+    多个 animateMotion 并行时，后者会覆盖前者
+    path 用法：查看
+    
+```
+- SVG 压缩 https://github.com/svg/svgo
+- SVG 实用工具 termtosvg https://github.com/nbedos/termtosvg
--- src/views/base/SvgAnimation/SvgAnimation.vue
+++ src/views/base/SvgAnimation/SvgAnimation.vue
@@ -78,6 +78,141 @@
           </svg>
         </div>
 
+        <div class="container">
+          <svg width="200" height="200">
+            <rect x="0" y="0" width="100" height="100" fill="red">
+              <set attributeName="x" attributeType="XML" to="10" begin="1s" />
+              <set attributeName="x" attributeType="XML" to="20" begin="2s" />
+              <set attributeName="x" attributeType="XML" to="30" begin="3s" />
+              <set attributeName="x" attributeType="XML" to="40" begin="4s" />
+              <set attributeName="x" attributeType="XML" to="50" begin="5s" />
+            </rect>
+          </svg>
+          <svg width="500" height="200" viewBox="0 0 500 200">
+            <circle cx="0" cy="0" r="30" fill="blue" stroke="black" stroke-width="1">
+              <animate attributeName="cx" from="0" to="200" dur="5s" repeatCount="indefinite" />
+              <animate attributeName="cy" from="0" to="200" dur="5s" repeatCount="indefinite" />
+            </circle>
+          </svg>
+          <svg width="400" height="400">
+            <polygon points="30 30 70 30 90 70 10 70" fill="#fcc" stroke="black">
+              <animate attributeName="points" attributeType="XML" to="50 30 70 50 50 90 30 50" dur="5s" fill="freeze" repeatCount="1" />
+            </polygon>
+          </svg>
+
+          <svg width="200" height="200" viewBox="0 0 200 200">
+            <rect x="0" y="0" width="60" height="60" fill="red">
+              <animateTransform attributeName="transform" begin="0s" dur="3s" type="scale" from="1" to="2" repeatCount="indefinite" />
+            </rect>
+          </svg>
+        </div>
+        <div class="container">
+          <svg width="200" height="200" viewBox="0 0 200 200">
+            <rect x="0" y="0" width="10" height="10" fill="red">
+              <animateMotion
+                path="M 10 10 L 110 10 L 110 110 L 10 110 Z"
+                dur="5s"
+                rotate="auto"
+                fill="freeze"
+                repeatCount="indefinite"
+              />
+            </rect>
+            <path id="motion-path" d="M 10 10 L 110 10 L 110 110 L 10 110 Z" fill="none" stroke="green" />
+          </svg>
+
+          <svg width="200" height="200" viewBox="0 0 200 200">
+            <rect x="0" y="0" fill="red" width="10" height="10">
+              <animateMotion
+                path="M10 10 L110 10 L110 110 L10 110 Z"
+                dur="5s"
+                rotate="0"
+                fill="freeze"
+                repeatCount="indefinite"
+              />
+            </rect>
+            <path d="M10 10 L110 10 L110 110 L10 110 Z" fill='none' stroke="green"></path>
+          </svg>
+
+          <svg viewBox="0 0 200 200" width="200" height="200">
+              <rect x="0" y="0" rx="0" ry="0" width="10" height="10" fill="red">
+                <animateMotion
+                  id="forward-rect"
+                  path="M 10 10 L 110 10 L 110 110 L 10 110"
+                  dur="2s"
+                  rotate="0"
+                  fill="freeze"
+                  begin="0; backward-rect.end + 0.5s"
+                />
+                <animateMotion
+                  id="backward-rect"
+                  path="M 10 110 L 110 110 L 110 10 L 10 10"
+                  dur="2s"
+                  rotate="0"
+                  fill="freeze"
+                  begin="forward-rect.end + 0.5s"
+                />
+                <animate
+                  id="red-to-blue"
+                  attributeType="XML"
+                  attributeName="fill"
+                  begin="0; blue-to-red.end + 1s"
+                  from="red"
+                  to="blue"
+                  dur="2s"
+                  fill="freeze"
+                />
+                <animate
+                  id="blue-to-red"
+                  attributeType="XML"
+                  attributeName="fill"
+                  begin="red-to-blue.end + 1s"
+                  from="blue"
+                  to="red"
+                  dur="2s"
+                  fill="freeze"
+                />
+              </rect>
+              <path d="M 10 10 L 110 10 L 110 110 L 10 110" fill="none" stroke-width="1" stroke="blue"/>
+          </svg>
+
+          <svg viewBox="0 0 200 200" width="200" height="200">
+              <g id="rect1">
+                <rect x="0" y="0" rx="0" ry="0" width="100" height="100" fill="red">
+                  <animate
+                    attributeType="XML"
+                    attributeName="fill"
+                    from="red"
+                    to="green"
+                    begin="rect1.click"
+                    dur="2s"
+                    fill="freeze"
+                  />
+                </rect>
+              </g>
+              <animateTransform
+                attributeType="XML"
+                attributeName="transform"
+                type="translate"
+                from="0, 0"
+                to="50, 50"
+                begin="rect1.click"
+                dur="2s"
+                fill="freeze"
+              />
+              <rect x="0" y="100" width="100" height="100" fill="blue">
+                <animate
+                  attributeType="XML"
+                  attributeName="fill"
+                  from="blue"
+                  to="green"
+                  begin="rect1.click"
+                  dur="2s"
+                  fill="freeze"
+                />
+              </rect>
+          </svg>
+        </div>
+
     </div>
 
 </template>
